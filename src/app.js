ceeb81ccf699dccb8f41a3dc59eeb00bcf8afc27
Merpels <yuriim0619@gmail.com>
2022-05-06 00:33:26 +0900

    search engine

--- src/app.js
+++ src/app.js
@@ -26,14 +26,27 @@ function displayTemperature(response) {
   descriptionElement.innerHTML = response.data.weather[0].descriptionElement;
   humidityElement.innerHTML - response.data.main.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
-  dateElement.innerHTML = formateDate(response.data.date * 1000);
+  dateElement.innerHTML = formatDate(response.data.date * 1000);
   iconElement.setAttribute(
     "src",
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
 }
-let apiKey = "bde4594240bbd5634acfce653024105f";
-let city = "London";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-axios.get(apiUrl).then(displayTemperature);
+
+function search(city) {
+  let apiKey = "bde4594240bbd5634acfce653024105f";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
+
+search("New York");
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
